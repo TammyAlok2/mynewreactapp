cce35e955cac123faa268d0a9d85da0cee24b513
Alok Tamrakar <aloktamrakar2@gmail.com>
2023-01-14 08:38:39 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./styles.css";
 
+let a = "alok tamrakar";
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>
+        Start editing to see some magic happen! This is me {a}
+        This is me here for the learning the react this is me first day to start
+        this program I hope i complete this program with great enthusiasm{" "}
+      </h2>
     </div>
   );
 }
